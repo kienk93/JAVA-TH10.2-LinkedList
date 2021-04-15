ed2bea6c1d4c0c6f28e880ad264ca4a1d73d1afd
kienk93 <kienk93@users.noreply.github.com>
2021-04-15 14:40:55 +0700

    'add'

---
+++ TH10second/MyLinkedListTest.java
@@ -0,0 +1,14 @@
+package TH10second;
+
+public class MyLinkedListTest {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+        ll.add(3,1);
+        ll.add(2,2);
+        ll.printList();
+    }
+}
